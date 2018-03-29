188fb00db3bd2ea8b08c3da9bac5db016e3b8c25
Preetam Jain <jain.pree@husky.neu.edu>
2018-03-29 11:17:27 -0400

    Adding class assignment eventEmitter

---
+++ src/app/child1/child1.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child1',
+  templateUrl: './child1.component.html',
+  styleUrls: ['./child1.component.css']
+})
+export class Child1Component implements OnInit {
+  @Output() onButtonClicked = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  buttonClicked() {
+    this.onButtonClicked.emit('Hi!!!');
+  }
+}
